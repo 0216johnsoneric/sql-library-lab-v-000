536c2a382507280c8e634afe9bcfa634ac1291c6
Eric Johnson <ericjohnson0216@icloud.com>
2018-06-10 00:13:53 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE series (
     author_id INTEGER,
     subgenre_id INTEGER
 );
+
+CREATE TABLE subgenres (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    author_id INTEGER,
+    subgenre_id INTEGER
+);
